0dfac0fd9bad56851d1da630ebf03eaf5f5ffb55
acareaga <aaroncareaga@gmail.com>
2015-09-21 16:37:06 -0600

    Built out part of district repository.

---
+++ lib/district_repository.rb
@@ -0,0 +1,37 @@
+require 'csv'
+require 'pry'
+
+class DistrictRepository
+
+  attr_reader :district,
+
+  def initialize
+    @repo_data = repo_data
+  end
+
+  def find_by_name(district)
+    # returns either nil or an instance of District having done a case insensitive search
+    District.new || nil
+  end
+
+  def find_all_matching
+    #returns either [] or one or more matches which contain the supplied name fragment, case insensitive
+  end
+
+  def economic_profile
+    repo_data
+    # collection of data sets for each district
+    # move to class eventually
+  end
+
+  # recieves a String
+  # returns a DistrictRepository
+  def self.from_csv(path)
+    filename  = 'Students qualifying for free or reduced price lunch.csv'
+    fullpath  = File.join path, filename
+    rows      = CSV.read(fullpath, headers: true, header_converters: :symbol) #.map { hash stuff }
+    repo_data = rows(district, year)
+
+    DistrictRepository.new
+  end
+end
