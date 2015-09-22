30e823f182f1209cd298088958b76696f0905ffb
acareaga <aaroncareaga@gmail.com>
2015-09-22 10:41:07 -0600

    Notes from hc decomp.

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -10,13 +10,15 @@ class DistrictRepository
   end
 
   def find_by_name(district)
+    # recieves a String
     # returns either nil or an instance of District having done a case insensitive search
     # make sure to downcase
     District.new || nil
   end
 
   def find_all_matching(fragment)
-    #returns either [] or one or more matches which contain the supplied name fragment, case insensitive
+    # recieves a String
+    # returns either [] or one or more matches which contain the supplied name fragment, case insensitive
     # make sure to downcase
   end
 
@@ -26,12 +28,13 @@ class DistrictRepository
     # move to class eventually
   end
 
-  # recieves a String
-  # returns a DistrictRepository
   def self.from_csv(path)
+    # recieves a String
+    # returns a DistrictRepository
     filename  = 'Students qualifying for free or reduced price lunch.csv'
     fullpath  = File.join path, filename
-    rows      = CSV.read(fullpath, headers: true, header_converters: :symbol).map { hash stuff }
+    rows      = CSV.read(fullpath, headers: true, header_converters: :symbol).map {
+                  |row| [row.fetch(:key), row.fetch(:value)] }.map(&:to_h)
     repo_data = rows(district, year)
 
     DistrictRepository.new
