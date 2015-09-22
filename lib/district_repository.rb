ca17050e003a2fcc0932eee29e6eb60bde021015
acareaga <aaroncareaga@gmail.com>
2015-09-21 21:39:01 -0600

    Added a few notes on dist repo.

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -11,11 +11,13 @@ class DistrictRepository
 
   def find_by_name(district)
     # returns either nil or an instance of District having done a case insensitive search
+    # make sure to downcase
     District.new || nil
   end
 
-  def find_all_matching
+  def find_all_matching(fragment)
     #returns either [] or one or more matches which contain the supplied name fragment, case insensitive
+    # make sure to downcase
   end
 
   def economic_profile
@@ -29,7 +31,7 @@ class DistrictRepository
   def self.from_csv(path)
     filename  = 'Students qualifying for free or reduced price lunch.csv'
     fullpath  = File.join path, filename
-    rows      = CSV.read(fullpath, headers: true, header_converters: :symbol) #.map { hash stuff }
+    rows      = CSV.read(fullpath, headers: true, header_converters: :symbol).map { hash stuff }
     repo_data = rows(district, year)
 
     DistrictRepository.new
