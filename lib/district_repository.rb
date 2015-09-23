81c3ede70e6fcf43f52d5ec47586f0682ce65f85
Matt Rooney <matthewjrooney@gmail.com>
2015-09-22 19:22:56 -0600

    Created test/data files for simpler testing. Currently pulling csv file through. Need to hard code our way through free or reduced lunch in year method.

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -1,31 +1,45 @@
-require 'csv'
-require 'pry'
+require 'csv'  # => true
+require 'pry'  # => true
 
 class DistrictRepository
 
-  attr_reader :district,
+  attr_reader :district, :repo_data
 
-  def initialize
+  def initialize(repo_data)
     @repo_data = repo_data
-  end
+  end                       # => nil
 
-  def find_by_name(district)
-    # recieves a String
+  def find_by_name(district_name)
+    @districts = repo_data.map { |row| [row.fetch(:location), row.fetch(:data)] }.to_h
+    @districts[district_name]
+    binding.pry
+    # if @memorized_districts[district] ||=
+    #   @memorized_districts[district]
+    # else @memorized_districts[district] =
+    #   @memorized_districts.find { |district| district.name == name}
+    # end
+    # @memorized_districts
+    # # recieves a String
     # returns either nil or an instance of District having done a case insensitive search
     # make sure to downcase
-    District.new || nil
-  end
+
+    # District.new || nil
+  end                                                               # => :find_by_name
 
   def find_all_matching(fragment)
     # recieves a String
     # returns either [] or one or more matches which contain the supplied name fragment, case insensitive
     # make sure to downcase
-  end
+  end                              # => :find_all_matching
 
   def economic_profile
-    repo_data
+    free_or_reduced_lunch_in_year
     # collection of data sets for each district
     # move to class eventually
+  end                   # => :economic_profile
+
+  def free_or_reduced_lunch_in_year(year)
+
   end
 
   def self.from_csv(path)
@@ -33,10 +47,11 @@ class DistrictRepository
     # returns a DistrictRepository
     filename  = 'Students qualifying for free or reduced price lunch.csv'
     fullpath  = File.join path, filename
-    rows      = CSV.read(fullpath, headers: true, header_converters: :symbol).map {
-                  |row| [row.fetch(:key), row.fetch(:value)] }.map(&:to_h)
-    repo_data = rows(district, year)
+    repo_data = CSV.read(fullpath, headers: true, header_converters: :symbol).map(&:to_h)
+    DistrictRepository.new(repo_data)
+  end            # => :from_csv
+end              # => :from_csv
 
-    DistrictRepository.new
-  end
-end
+Hash[:key, "value"]                # => {:key=>"value"}
+Hash[:key, Hash[:key2, "value"]]   # => {:key=>{:key2=>"value"}}
+[["a",1], ["b",2], ["c", 3]].to_h  # => {"a"=>1, "b"=>2, "c"=>3}
--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -1,15 +1,73 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/district_repository'
+require 'csv'
 require 'pry'
+
+class DistrictRepositoryTest < Minitest::Test
+  def test_district_repo_class_exists
+    repo_data = []
+    repo = DistrictRepository.new(repo_data)
+    assert_equal DistrictRepository, repo.class
+  end
+
+  def test_it_holds_districts
+    skip
+    repository = DistrictRepository.from_csv('./test/data')
+    repository.find_by_name
+  end
+
+  def test_it_can_find_by_name
+    skip
+    repository = DistrictRepository.from_csv('./test/data')
+    district = repository.find_by_name("ACADEMY 20")
+  end
+
+  def test_it_can_find_all_matching
+    skip
+    repository = DistrictRepository.from_csv('./test/data')
+    district = repository.find_all_matching("ACADEMY 20")
+  end
+
+  def test_find_by_name_returns_nil_when_dist_does_not_exist
+    skip
+    repository = DistrictRepository.from_csv('./test/data')
+    district = repository.find_by_name("DUHCADEMY 20")
+  end
+
+  def test_find_by_name_is_case_insensitive
+    skip
+    repository = DistrictRepository.from_csv('./test/data')
+    district = repository.find_by_name("Academy 20")
+  end
+
+  def test_find_all_matching_is_case_insensitive
+    skip
+    repository = DistrictRepository.from_csv('./test/data')
+    district = repository.find_all_matching("Academy 20")
+  end
+
+  def test_find_all_matching_with_incomplete_name
+    skip
+    repository = DistrictRepository.from_csv('./test/data')
+    district = repository.find_all_matching("Acad")
+  end
+
+  def test_find_all_matching_with_incomplete_name_returns_nil
+    skip
+    repository = DistrictRepository.from_csv('./test/data')
+    district = repository.find_all_matching("duh")
+  end
+end
+
 
 class TestEconomicProfile < Minitest::Test
 
   def test_free_or_reduced_lunch_in_year
-    path       = File.expand_path("../data", __dir__)
+    path       = File.expand_path("./data", __dir__)
     repository = DistrictRepository.from_csv(path)
     district   = repository.find_by_name("ACADEMY 20")
-
+    repo       =
     assert_equal 0.125, district.economic_profile.free_or_reduced_lunch_in_year(2012)
   end
 
