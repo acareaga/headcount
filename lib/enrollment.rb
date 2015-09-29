dfab57c513d75cb530eee949b62f25a6a4d9dc4d
Matt Rooney <matthewjrooney@gmail.com>
2015-09-29 13:14:28 -0600

    Add enrollment skeleton

---
+++ lib/enrollment.rb
@@ -0,0 +1,112 @@
+require './lib/district'
+require 'pry'
+
+class Enrollment
+  # Represents data from these files
+    # Dropout rates by race and ethnicity.csv
+    # High school graduation rates.csv
+    # Kindergartners in full-day program.csv
+    # Online pupil enrollment.csv
+    # Pupil enrollment by race_ethnicity.csv
+    # Pupil enrollment.csv
+    # Special education.csv
+    # Remediation in higher education.csv
+
+  attr_reader :district_data, :name
+
+  def initialize(name, district_data)
+    @name = name
+    @district_data = district_data
+    @dropout_rate_by_year = district_data
+      .select { |row| row.fetch(:category) == "All"}
+      .map { |column| [column.fetch(:timeframe).to_i, column.fetch(:data)]}
+  end
+
+  def dropout_rate_in_year(year)
+    # returns a truncated three-digit floating point number representing a percentage
+    # or nil for unknown year
+    @dropout_rate_by_year.values_at(year).pop
+  end
+
+  def dropout_rate_by_gender_in_year(year)
+    # returns a hash with genders as keys and three-digit floating point number representing a percentage.
+    # or nil
+  end
+
+  def dropout_rate_by_race_in_year(year)
+    # returns a hash with race markers as keys and a three-digit floating
+    #  point number representing a percentage or nil
+  end
+
+  def dropout_rate_for_race_or_ethnicity(race)
+    # race as a symbol from the following set:
+    # [:asian, :black, :pacific_islander, :hispanic, :native_american, :two_or_more, :white]
+    # returns a hash with years as keys and a three-digit floating
+  end
+
+  def dropout_rate_for_race_or_ethnicity_in_year(race, year)
+    # returns a truncated three-digit floating point number representing a percentage
+    # or nil
+  end
+
+  def graduation_rate_by_year
+    # returns a hash with years as keys and a truncated three-digit floating point number representing a percentage.
+  end
+
+  def graduation_rate_in_year(year)
+    # returns a truncated three-digit floating point number representing a percentage.
+    # or nil
+  end
+
+  def kindergarten_participation_by_year
+    # returns a hash with years as keys and a truncated three-digit floating point number representing a percentage
+  end
+
+  def kindergarten_participation_in_year(year)
+    # returns a truncated three-digit floating point number representing a percentage.
+    # or nil
+  end
+
+  def online_participation_by_year
+    # returns a hash with years as keys and an integer as the value
+  end
+
+  def online_participation_in_year(year)
+    # takes integer year, returns interger or nil for unknown year
+  end
+
+  def participation_by_year
+    # returns a hash with years as keys and an integer as the value
+  end
+
+  def participation_in_year(year)
+    # takes integer year, returns interger or nil for unknown year
+  end
+
+  def participation_by_race_or_ethnicity(race)
+    # takes race as a symbol from the following set: [:asian, :black, :pacific_islander, :hispanic, :native_american, :two_or_more, :white]
+    # returns a hash with years as keys and a three-digit floating point number representing a percentage
+    # or UnknownRaceError
+  end
+
+  def participation_by_race_or_ethnicity_in_year(year)
+    # returns a hash with race markers as keys and a three-digit floating point number representing a percentage.
+  end
+
+  def special_education_by_year
+    # returns a hash with years as keys and an floating point three-significant digits representing a percentage.
+  end
+
+  def special_education_in_year(year)
+    #  returns a single three-digit floating point percentage.
+  end
+
+  def remediation_by_year
+    # method returns a hash with years as keys and an floating point three-significant digits representing a percentage.
+  end
+
+  def remediation_in_year(year)
+    # method returns a single three-digit floating point percentage
+    # or nil for unknown year
+  end
+end
---
+++ test/enrollment_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enrollment'
+require 'pry'
+
+class EnrollmentTest < Minitest::Test
+
+  def test_dropout_rate_in_year
+    path       = File.expand_path("./data", __dir__)
+    repository = DistrictRepository.from_csv(path)
+    district   = repository.find_by_name("ACADEMY 20")
+
+    assert_equal 0.002, district.enrollment.dropout_rate_in_year(2011)
+  end
+end
